110c6f2f4004b1d2a7aa996d13b01ba8cb61cd38
Hecate2 <2474101468@qq.com>
2022-09-28 12:33:10 +0800

    if type(element) is list

--- neo_fairy_client/utils/misc.py
+++ neo_fairy_client/utils/misc.py
@@ -2,7 +2,7 @@ from typing import Any, List
 
 
 def to_list(element: Any) -> List:
-    if element is list:
+    if type(element) is list:
         return element
     if element is not None:
         return [element]
