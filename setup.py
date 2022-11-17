f08aff32be15ee788cac42c6e7101bddc275ee57
Hecate2 <2474101468@qq.com>
2022-11-17 16:27:40 +0800

    publish as pip package

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setuptools.setup(
+    name="neo-fairy-client",
+    version="3.4.0.1",
+    author="Hecate2",
+    author_email="hecate2@qq.com",
+    description="Test & debug your Neo3 smart contracts",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Hecate2/neo-fairy-client",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'requests>=2.27.1',
+        'neo_mamba==0.9',
+    ],
+    python_requires='>=3.8',
+)
