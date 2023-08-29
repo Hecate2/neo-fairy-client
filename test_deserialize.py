6c08f222f6a272014d303c882e609ff9cd7b9d6f
Hecate2 <2474101468@qq.com>
2023-08-29 12:03:34 +0800

    test deserialize with NEO AccountState

--- test_deserialize.py
+++ test_deserialize.py
@@ -1,6 +1,6 @@
-from typing import Dict, List, Tuple
+from typing import Dict, List, Tuple, Union
 import base64
-from neo_fairy_client import FairyClient, GasAddress, UInt160, Hash160Str
+from neo_fairy_client import FairyClient, NeoAddress, GasAddress, Hash160Str, PublicKeyStr
 
 client = FairyClient()
 
@@ -19,4 +19,23 @@ gas_storage: List[Tuple[Hash160Str, int]] = [
 for k, v in gas_storage:
     assert type(k) is Hash160Str
     assert type(v) is int
-print(len(gas_storage))
+print('GAS holders count:', len(gas_storage))
+
+neo_storage: Dict[str, str] = client.find_storage_with_session(b'\x14', None, NeoAddress)
+neo_storage: List[Tuple[Hash160Str, str]] = [
+    (FairyClient.bytes_to_Hash160Str(base64.b64decode(k)[1:]), v)
+    for k, v in neo_storage.items()
+]
+data = [(balance, since_block, PublicKeyStr(vote_to.hex()) if vote_to else None) for balance, since_block, vote_to in client.deserialize([v for _, v in neo_storage])]
+neo_storage: List[Tuple[Hash160Str, int, int, Union[None, PublicKeyStr]]] = [
+    (k, balance, since_block, vote_to) for (k, v), (balance, since_block, vote_to) in zip(neo_storage, data)
+]
+for k, balance, since_block, vote_to in neo_storage:
+    assert type(k) is Hash160Str
+    assert type(balance) is int
+    assert type(since_block) is int
+    assert vote_to is None or type(vote_to) is PublicKeyStr
+print('NEO holders count:', len(neo_storage))
+voted_neo = [(k, balance, since_block, vote_to) for (k, balance, since_block, vote_to) in neo_storage if vote_to]
+print('NEO voters count:', len(voted_neo))
+print('NEO voted balance:', sum([balance for (k, balance, since_block, vote_to) in voted_neo]))
